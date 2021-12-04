1c570d417a07db80379edfe5972be35d42d76f7d
RhysWood <rhyswoody@gmail.com>
2021-12-04 22:05:00 +0000

    refactored to be more readable

--- timer1.js
+++ timer1.js
@@ -1,11 +1,12 @@
-const arg = process.argv.slice(2);
+const arg = process.argv.slice(2); // argument coming from comandline - slicing off the beghinning info
 
 const timer = function(arg) {
-  arg.forEach(element => {
-    if (element > 0) {
-      setTimeout(() => {
-        process.stdout.write('\x07');
-      }, (element * 1000));
+  arg.forEach(element => { // for each comandline arg, 
+    const isValidNumber = element > 0 || typeof element === 'number';
+    if (isValidNumber) { //error handling - so nobody can put in a number <0 or different data type
+      setTimeout(() => { //async 
+        process.stdout.write('\x07'); // The function 'beep' is called
+      }, (element * 1000)); //the delay
     }
   });
 };
